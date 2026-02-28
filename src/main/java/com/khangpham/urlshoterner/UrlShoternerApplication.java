bf817204ff25a2fdb77fcb2dfa2300fd6dcd5ac9
NKhang1234 <nhatkhangpham130304@gmail.com>
2026-02-28 14:59:10 +0700

    First commit

---
+++ src/main/java/com/khangpham/urlshoterner/UrlShoternerApplication.java
@@ -0,0 +1,13 @@
+package com.khangpham.urlshoterner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShoternerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShoternerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khangpham/urlshoterner/UrlShoternerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khangpham.urlshoterner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShoternerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
